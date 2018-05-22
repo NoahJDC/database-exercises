fced5ce8e50dc7b58b86e08e47b7df4e6d300fb3
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-22 15:48:15 -0500

    addded TRUNCATE album to album_seeder.sql and converted SELECT to DELETE for delete_exercise.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
   id int UNSIGNED NOT NULL AUTO_INCREMENT,
   artist VARCHAR(100) NOT NULL,
-  name VARCHAR(50) NOT NULL,
+  name VARCHAR(100) NOT NULL,
   release_date INT UNSIGNED,
   sales FLOAT(6,2),
-  genre VARCHAR(20),
+  genre VARCHAR(50),
   PRIMARY KEY(id)
  );
