79d3c6d6c0186b19114a5e015b171eedfeebb77d
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-22 10:35:42 -0500

    added albums table

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+CREATE TABLE albums(
+  id int UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(100) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  release_date DATE,
+  sales INT,
+  genre VARCHAR(20),
+  PRIMARY KEY(id)
+ );
