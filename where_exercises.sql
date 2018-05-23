5dd34a22bb601b445d96bc5453ccecff7ad05e80
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-23 14:24:04 -0500

    added additional AND/OR

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,12 +1,21 @@
 USE employees;
 
-SELECT first_name
+SELECT *
 FROM Employees
-WHERE first_name in ('Irena','Vidya', 'Maya');
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya'
+AND gender = 'M';
 
 SELECT last_name
 FROM Employees
-WHERE last_name LIKE 'e%';
+WHERE last_name LIKE 'e%'
+AND last_name like '%e';
+
+SELECT last_name
+FROM Employees
+WHERE last_name LIKE 'e%'
+OR last_name like '%e';
 
 SELECT hire_date
 FROM employees
@@ -18,9 +27,16 @@ WHERE birth_date like '%-12-25';
 
 SELECT last_name
 from employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
+
+SELECT first_name
+FROM employees
+WHERE hire_date like '199%'
+AND birth_date like '%-12-25';
 
 
--- SELECT *
+-- SELECT first_name
 -- FROM employees
--- WHERE birth_date BETWEEN '1970-01-01' and '1980-01-01';
+-- WHERE hire_date BETWEEN '199%-12-25' and '1999-12-25'
+-- AND  birth_date like '%-12-25';
