73e1e923db140e1edd2ec6597db1a6d2b3016a10
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-23 10:11:42 -0500

    added captions

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,15 +1,20 @@
 USE codeup_test_db;
-
+SELECT 'Albums by Pink Floyd' as 'Info';
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
+SELECT 'This song was released' as 'Info';
 SELECT release_date from albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
+SELECT 'Genre of Nevermind' as 'Info';
 SELECT genre from albums WHERE name = 'Nevermind';
 
-SELECT NAME FROM albums WHERE release_date > 1990 < 2000;
+SELECT 'All albums released between 1990 and 1999' as 'Info';
+SELECT NAME FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
+SELECT 'Albums with less that 20 mil sales' as 'Info'
 SELECT NAME FROM albums WHERE sales < 20.0;
 
--- SELECT NAME FROM albums where artists LIKE '%rock%'; ( to find everything with rock in the genre col)
+-- SELECT NAME FROM albums where artists LIKE '%rock%'; ( to find everything with rock in the genre column)
+SELECT 'All rock albums' as 'Info';
 SELECT NAME FROM albums WHERE genre = "Rock";
 
