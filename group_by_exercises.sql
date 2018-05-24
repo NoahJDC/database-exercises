6db567ef278aea88aa5080cf285d658155ce154c
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-24 11:32:22 -0500

    Found unique last names with q but not qu

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -8,3 +8,9 @@ FROM Employees
 WHERE last_name LIKE 'e%'
       AND last_name like '%e'
       GROUP BY first_name,last_name;
+
+SELECT last_name
+from employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
