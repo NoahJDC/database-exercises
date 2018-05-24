738b3df0a5b50484b18a9a313df445537036e004
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-24 11:46:09 -0500

    updated query to find number of employees for each gender with specific name

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -9,8 +9,15 @@ WHERE last_name LIKE 'e%'
       AND last_name like '%e'
       GROUP BY first_name,last_name;
 
-SELECT last_name
+SELECT last_name, count(last_name)
 from employees
 WHERE last_name LIKE '%q%'
       AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
+
+SELECT concat_ws(' ',count(gender),gender) as 'Genders'
+FROM Employees
+WHERE (first_name = 'Irena'
+      OR first_name = 'Vidya'
+      OR first_name = 'Maya')
+      GROUP BY gender;
